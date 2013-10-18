3f6a90682e45bbe55d59eb239ea6cdc24981e4af
Sean Freiburg <frycicle@gmail.com>
2013-10-17 20:53:00 -0500

    latest

--- test_outputs.rb
+++ test_outputs.rb
@@ -12,6 +12,13 @@ real_key = '/api/players/a3affc74-9f1f-4cee-a9cc-cce0204ce0cb'
 main(real_key)
 
 
+puts "Enter an action (Call, Fold, Bet ) and amount bet 50"
+input = gets.chomp.strip 
+inputarray = input.split(" ") 
+inputarray[0] = inputarray[0].downcase 
+puts inputarray
+
+
 #data = {"name"=>"Bill16", "your_turn"=>true, "initial_stack"=>250, "stack"=>250, "current_bet"=>nil, "call_amount"=>10, "hand"=>["TS", "TS"], "betting_phase"=>"deal", "players_at_table"=>[{"player_name"=>"Bill16", "initial_stack"=>250, "current_bet"=>0, "stack"=>250, "folded"=>false, "actions"=>[]}, {"player_name"=>"Bill17", "initial_stack"=>250, "current_bet"=>5, "stack"=>245, "folded"=>false, "actions"=>[{"action"=>"ante", "amount"=>5}]}, {"player_name"=>"Bill18", "initial_stack"=>250, "current_bet"=>10, "stack"=>240, "folded"=>false, "actions"=>[{"action"=>"ante", "amount"=>10}]}], "total_players_remaining"=>3, "table_id"=>427, "round_id"=>447, "round_history"=>[{"round_id"=>447, "table_id"=>427, "stack_change"=>nil}], "lost_at"=>nil, "community_cards"=>[]}
 
 #puts get_river_action(data)
